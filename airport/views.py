12a1e7ad862d8637f604de8e1ff906dd2499cd96
Michał Ołtarzewski <mi.oltarzewski@gmail.com>
2018-04-19 19:22:29 +0200

    Working log in, register and logout

--- airport/views.py
+++ airport/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
 from django.views.decorators.http import require_POST
+from django.contrib.auth import authenticate, login as login_to_session
+from django.contrib.auth.views import logout as djangoLogout
+from django.contrib.auth.models import User
 
 
 def flights_list(request):
@@ -13,14 +16,26 @@ def flight(request, flightId):
 
 @require_POST
 def register(request):
-    return HttpResponse("Register")
+    if User.objects.all().filter(username=request.POST['username']).exists():
+        registerError = True
+    else:
+        user = User.objects.create_user(
+            username=request.POST['username'], password=request.POST['password'])
+        login_to_session(request, user)
+    return render(request, 'main.html', locals())
 
 
 @require_POST
 def login(request):
-    return HttpResponse("Login")
+    user = authenticate(username=request.POST['username'], password=request.POST['password'])
+    if user is not None:
+        login_to_session(request, user)
+    else:
+        loginError = True
+    return render(request, 'main.html', locals())
 
 
 def logout(request):
-    return HttpResponse("Logout")
+    djangoLogout(request)
+    return render(request, 'main.html', locals())
 

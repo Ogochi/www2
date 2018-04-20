6a57e93daa1b3fa60a6b76c817e87558da7da964
Michał Ołtarzewski <mi.oltarzewski@gmail.com>
2018-04-20 13:35:34 +0200

    Added models validation

--- airport/admin.py
+++ airport/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from .models import Airplane, Passenger, Flight, FlightPassenger
+from .models import Airplane, Passenger, Flight, Ticket
 
 admin.site.register(Airplane)
 admin.site.register(Passenger)
 admin.site.register(Flight)
-admin.site.register(FlightPassenger)
+admin.site.register(Ticket)
--- airport/models.py
+++ airport/models.py
@@ -1,9 +1,16 @@
 from django.db import models
+from django.db.models import Q
+from django.core.exceptions import ValidationError
+from datetime import timedelta
 
 
 class Airplane(models.Model):
     registerNumber = models.CharField(max_length=255, unique=True)
     places = models.IntegerField()
+
+    def clean(self):
+        if self.places < 0:
+            raise ValidationError('Airplane can not have less than 0 places!')
 
     def __str__(self):
         return 'Airplane %s' % self.registerNumber
@@ -12,6 +19,9 @@ class Airplane(models.Model):
 class Passenger(models.Model):
     name = models.CharField(max_length=255)
     surname = models.CharField(max_length=255)
+
+    class Meta:
+        unique_together = ('name', 'surname')
 
     def __str__(self):
         return 'Passenger %s %s' % (self.name, self.surname)
@@ -22,11 +32,39 @@ class Flight(models.Model):
     endAirport = models.CharField(max_length=255)
     startTime = models.DateTimeField()
     endTime = models.DateTimeField()
+    airplane = models.ForeignKey(Airplane, on_delete=models.CASCADE)
+
+    def clean(self):
+        if self.endTime < self.startTime:
+            raise ValidationError('End time is before start time!')
+        dateDiff = self.endTime - self.startTime
+        if dateDiff < timedelta(minutes=30):
+            raise ValidationError('Flight is shorter than 30 min!')
+
+        for flight in Flight.objects.all().filter(airplane=self.airplane):
+            if flight.startTime <= self.startTime <= flight.endTime \
+                    or flight.startTime <= self.endTime <= flight.endTime:
+                raise ValidationError('Airplane can not have two flights in the same time!')
+
+        flightsInStartDay = Flight.objects.all().filter(
+            Q(startTime__day=self.startTime.day) | Q(endTime__day=self.startTime.day))
+        if flightsInStartDay.values('pk').distinct().count() == 4:
+            raise ValidationError('Can not have more than 4 flights in the same day!')
+        flightsInEndDay = Flight.objects.all().filter(
+            Q(startTime__day=self.endTime.day) | Q(endTime__day=self.endTime.day))
+        if flightsInEndDay.values('pk').distinct().count() == 4:
+            raise ValidationError('Can not have more than 4 flights in the same day!')
 
     def __str__(self):
         return 'Flight from %s to %s' % (self.startAirport, self.endAirport)
 
 
-class FlightPassenger(models.Model):
+class Ticket(models.Model):
     flight = models.ForeignKey(Flight, on_delete=models.CASCADE)
     passenger = models.ForeignKey(Passenger, on_delete=models.CASCADE)
+
+    def clean(self):
+        currPassengersNum = Ticket.objects.all().filter(flight=self.flight).count()
+        airplanePlaces = Flight.objects.get(pk=self.flight.pk).airplane.places
+        if currPassengersNum == airplanePlaces:
+            raise ValidationError('There are not empty places in airplane!')
